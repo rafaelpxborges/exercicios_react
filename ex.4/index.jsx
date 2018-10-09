4174f990052f0a4b66e7ef75376ad6a15131b90e
rafaelpxborges <rafaelpxborges@gmail.com>
2018-10-08 21:56:08 -0300

    Initial commit

---
+++ ex.4/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Primeiro, Segundo  } from './component.jsx'
+
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+        <Segundo></Segundo>
+    </div>
+
+, document.getElementById('app'))
---
+++ ex.5/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SilvaFamily from './silviaFamily'
+
+
+ReactDOM.render(
+    <SilvaFamily />
+
+, document.getElementById('app'))
